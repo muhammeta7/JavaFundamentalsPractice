4e418d445e5044472d406e78873b512ad89519c1
muhammeta7 <muhammeta7@gmail.com>
2020-05-25 17:52:09 -0400

    added tests for sorting string of ints

--- Java-Fundamentals/src/test/java/com/JavaFundamentals/strings/SortStringOfIntsTest.java
+++ Java-Fundamentals/src/test/java/com/JavaFundamentals/strings/SortStringOfIntsTest.java
@@ -4,4 +4,43 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class SortStringOfIntsTest {
+    private StringUtils stringUtils;
+
+    @Before
+    public void setup() {
+        this.stringUtils = new StringUtils();
+    }
+
+    @Test
+    public void test0(){
+        String[] input = {"3","2","4","10","11","6","5","8","9","7"};
+        String[] expected = {"2","3","4","5","6","7","8","9","10","11"};
+        String[] actual = stringUtils.sortStringOfInts(input);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test1(){
+        String[] input = {"50","22","4","11","12","6"};
+        String[] expected = {"4", "6", "11", "12", "22", "50"};
+        String[] actual = stringUtils.sortStringOfInts(input);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test2(){
+        String[] input = {"100", "50", "150", "300"};
+        String[] expected = {"50", "100", "150", "300"};
+        String[] actual = stringUtils.sortStringOfInts(input);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test3(){
+        String[] input = {"5", "4", "3", "2", "1"};
+        String[] expected = {"1", "2", "3", "4", "5"};
+        String[] actual = stringUtils.sortStringOfInts(input);
+        assertArrayEquals(expected, actual);
+    }
 }
+
