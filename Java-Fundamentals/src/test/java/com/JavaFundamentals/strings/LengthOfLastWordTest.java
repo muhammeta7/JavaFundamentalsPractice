2412dbbd74d3f0a855094f65a29e2490ab12f0e7
muhammeta7 <muhammeta7@gmail.com>
2020-05-29 18:09:10 -0400

    added tests

--- Java-Fundamentals/src/test/java/com/JavaFundamentals/strings/LengthOfLastWordTest.java
+++ Java-Fundamentals/src/test/java/com/JavaFundamentals/strings/LengthOfLastWordTest.java
@@ -11,24 +11,28 @@ public class LengthOfLastWordTest {
         this.stringUtils = new StringUtils();
     }
 
+    @Test
     public void test0(){
         int expected = 0;
         int actual = stringUtils.lengthOfLastWord("");
         assertEquals(expected, actual);
     }
 
+    @Test
     public void test1(){
         int expected = 5;
         int actual = stringUtils.lengthOfLastWord("Hello World");
         assertEquals(expected, actual);
     }
 
+    @Test
     public void test2(){
         int expected = 2;
         int actual = stringUtils.lengthOfLastWord("There she is");
         assertEquals(expected, actual);
     }
 
+    @Test
     public void test3(){
         int expected = 4;
         int actual = stringUtils.lengthOfLastWord("hahaha this is not cool");
