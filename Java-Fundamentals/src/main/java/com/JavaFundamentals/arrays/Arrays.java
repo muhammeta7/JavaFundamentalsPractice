18cec8d9167c0aefa6ae1ef93b8633ea825d6565
muhammeta7 <muhammeta7@gmail.com>
2020-05-30 16:16:42 -0400

    remove element exercise

--- Java-Fundamentals/src/main/java/com/JavaFundamentals/arrays/Arrays.java
+++ Java-Fundamentals/src/main/java/com/JavaFundamentals/arrays/Arrays.java
@@ -53,4 +53,12 @@ public class Arrays {
     public String longestCommonPrefix(String[] strings){
         return null;
     }
+
+    // Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+    // Do not allocate extra space for another array, you must do this by modifying
+    // the input array in-place with O(1) extra memory.
+    // The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+    public int removeElement(int[] nums, int val){
+        return 0;
+    }
 }
