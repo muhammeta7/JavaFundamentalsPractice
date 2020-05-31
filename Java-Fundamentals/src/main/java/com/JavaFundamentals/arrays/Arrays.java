51066d7e6ff094c96aa1031df6d137a0c93dbc5d
muhammeta7 <muhammeta7@gmail.com>
2020-05-31 03:21:11 -0400

    added search insert

--- Java-Fundamentals/src/main/java/com/JavaFundamentals/arrays/Arrays.java
+++ Java-Fundamentals/src/main/java/com/JavaFundamentals/arrays/Arrays.java
@@ -61,4 +61,11 @@ public class Arrays {
     public int removeElement(int[] nums, int val){
         return 0;
     }
+
+    // Given a sorted array and a target value, return the index if the target is found.
+    // If not, return the index where it would be if it were inserted in order.
+    // You may assume no duplicates in the array.(Hint: Binary Search)
+    public int searchInsert(int[] nums, int target){
+        return 0;
+    }
 }
