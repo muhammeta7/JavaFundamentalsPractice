d8e0e0d022c5c8decfd1189e8fd85291d9196342
muhammeta7 <muhammeta7@gmail.com>
2020-06-01 21:29:30 -0400

    completed generics bubble sort

--- Java-Fundamentals/src/main/java/com/JavaFundamentals/generics/Generics.java
+++ Java-Fundamentals/src/main/java/com/JavaFundamentals/generics/Generics.java
@@ -3,7 +3,26 @@ package com.JavaFundamentals.generics;
 public class Generics<T extends Comparable<? super T>> {
 
     public T[] bubbleSort(T[] array){
-        return null;
+        int n = array.length;
+        while (n > 0) {
+            int lastModifiedIndex = 0;
+            for (int currentIndex = 1; currentIndex < n; currentIndex++)
+            {
+                // if the item at the previous index is greater than the item at the `currentIndex`, swap them
+                if (array[currentIndex - 1].compareTo(array[currentIndex]) > 0)
+                {
+                    // swap
+                    T temp = array[currentIndex - 1];
+                    array[currentIndex - 1] = array[currentIndex];
+                    array[currentIndex] = temp;
+                    // save the index that was modified
+                    lastModifiedIndex = currentIndex;
+                }
+            }
+            // save the last modified index so we know not to iterate past it since all proceeding values are sorted
+            n = lastModifiedIndex;
+        }
+        return array;
     }
 
     public T binarySearch(T[] array, T value){
