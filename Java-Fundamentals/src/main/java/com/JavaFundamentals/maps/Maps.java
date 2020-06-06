bc25e0bbbbf11550cd6f5e0a83ecac56edb66f31
muhammeta7 <muhammeta7@gmail.com>
2020-06-06 03:31:17 -0400

    added helper function

--- Java-Fundamentals/src/main/java/com/JavaFundamentals/maps/Maps.java
+++ Java-Fundamentals/src/main/java/com/JavaFundamentals/maps/Maps.java
@@ -1,6 +1,8 @@
 package com.JavaFundamentals.maps;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Maps {
     // Given List of Anagrams and non anagram values.
@@ -8,5 +10,14 @@ public class Maps {
     // Return the list in alphabetical order
     public List<String> uniqueAnagrams(List<String> text){
         return null;
+    }
+
+    public static int numericalValue(String input){
+        char[] array = input.toCharArray();
+        int total = 0;
+        for(char c: array){
+            total += Math.pow((int)c, 2);
+        }
+        return total;
     }
 }
