3f328dddf50d98cb772193ef70891a095e15b5e4
muhammeta7 <muhammeta7@gmail.com>
2020-06-15 02:12:48 -0400

    added new stream exercise

--- Java-Fundamentals/src/main/java/com/JavaFundamentals/streams/Streams.java
+++ Java-Fundamentals/src/main/java/com/JavaFundamentals/streams/Streams.java
@@ -1,9 +1,17 @@
 package com.JavaFundamentals.streams;
 
+import java.util.List;
+
 public class Streams {
     // Return minimum number of deletions to make two strings anagrams
     // String contain only lowercase letters
     public int makingAnagrams(String one, String two){
         return 0;
+    }
+
+    // Given a list of strings, write a method that returns a list of all strings
+    // that start with the letter 'a' (lower case) and have exactly 3 letters.
+    public List<String> length3AndStartsWithA(List<String> input) {
+        return null;
     }
 }
