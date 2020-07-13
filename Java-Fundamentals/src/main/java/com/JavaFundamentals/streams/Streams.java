9485f065db66d6da6456132674f1b20d29133af4
muhammeta7 <muhammeta7@gmail.com>
2020-07-13 15:19:12 -0400

    added more exercises for interview prep

--- Java-Fundamentals/src/main/java/com/JavaFundamentals/streams/Streams.java
+++ Java-Fundamentals/src/main/java/com/JavaFundamentals/streams/Streams.java
@@ -42,11 +42,12 @@ public class Streams {
 
     // Write a method that returns a comma-separated string based on a given list of integers.
     // Each element should be preceded by the letter 'e' if the number is even, and preceded by the letter 'o'
-    // if the number is odd. For example, if the input list is (3,44), the output should be 'o3,e44'.
+    // if the number is odd. For example, if the input list is (3,44), the output should be '3o,44e'.
     public String evenOrOdd(List<Integer> input){
         return input.stream()
                 .map(i -> i % 2 == 0 ? i + "e" : i + "o")
                 .collect(joining(","));
     }
+
 
 }
