d305b4835fd9b31f3877af78fd5890a1c3c598c0
muhammeta7 <muhammeta7@gmail.com>
2020-07-15 22:10:30 -0400

    added tests

--- Java-Fundamentals/src/test/java/interview/InterviewTest.java
+++ Java-Fundamentals/src/test/java/interview/InterviewTest.java
@@ -1,9 +1,7 @@
 package interview;
-
 import com.JavaFundamentals.interview.Interview;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class InterviewTest {
